4ff16f0e1cc22257bb44e4c97b15f59c840b962b
Jaidin Hamilton <45185343+AkoniMinker@users.noreply.github.com>
2024-10-09 15:32:44 -0400

    Updated __init__.py and calc.py

--- ohms_law/calc.py
+++ ohms_law/calc.py
@@ -13,3 +13,12 @@ def calcResistance(voltage, current):
 class Drive:
     def __init__(self):
         pass
+        
+    def calcVoltage(self, current, resistance):
+        return calcVoltage(current, resistance)
+    
+    def calcCurrent(self, voltage, resistance):
+        return calcCurrent(voltage, resistance)
+    
+    def calcResistance(self, voltage, current):
+        return calcResistance(voltage, current)
